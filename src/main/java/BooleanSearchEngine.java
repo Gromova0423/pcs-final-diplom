9523264fa0a26617b140777c3122661c327f7612
Gromova0423 <gromova0423@gmail.com>
2023-07-02 17:49:08 +0300

    One commit

--- src/main/java/BooleanSearchEngine.java
+++ src/main/java/BooleanSearchEngine.java
@@ -1,18 +1,75 @@
+import com.itextpdf.kernel.pdf.PdfDocument;
+import com.itextpdf.kernel.pdf.PdfPage;
+import com.itextpdf.kernel.pdf.PdfReader;
+import com.itextpdf.kernel.pdf.canvas.parser.PdfTextExtractor;
+
 import java.io.File;
 import java.io.IOException;
 import java.util.*;
 
 public class BooleanSearchEngine implements SearchEngine {
-    //???
+    private Map<String, List<PageEntry>> index; // обратный индекс
+
 
     public BooleanSearchEngine(File pdfsDir) throws IOException {
-        // прочтите тут все pdf и сохраните нужные данные,
-        // тк во время поиска сервер не должен уже читать файлы
+        pdfsDir = new File("D:\\NETOLOGIA\\pcs-final-diplom\\pcs-final-diplom\\pdfs");
+        File[] files = pdfsDir.listFiles();
+
+
+        for (File file : files) {
+            if (file.isFile() && file.getName().endsWith(".pdf")) {
+                PdfReader reader = new PdfReader(file);
+                PdfDocument doc = new PdfDocument(reader);
+                for (int i = 1; i <= doc.getNumberOfPages(); i++) {
+                    PdfPage page = doc.getPage(i);
+                    String text = PdfTextExtractor.getTextFromPage(page); // извлекаем текст со страницы
+                    String[] wordsArray = text.split("\\P{IsAlphabetic}+");
+                    ArrayList<String> words = new ArrayList<>(Arrays.asList(wordsArray));// делим текст на слова \\P{IsAlphabetic}+ означает "любой символ, который не является буквой"
+
+
+                    Map<String, Integer> freqs = new HashMap<>(); // мапа, где ключом будет слово, а значением - частота. Пример мапы: (после=2, актуализация=1, последующей=2, рекламные=1)
+                    for (var word : words) {
+                        if (word.isEmpty()) {
+                            continue;
+                        }
+                        word = word.toLowerCase();
+                        freqs.put(word, freqs.getOrDefault(word, 0) + 1);  //freqs - словарь, где ключами являются уникальные слова из списка words, а значениями - количество вхождений каждого слова в тексте.
+                    }
+
+
+                    index = new HashMap<>();
+                    for (var word : words) { // перебираем слова в массиве
+                        if (word.isEmpty()) {
+                            continue;
+                        }
+                        word = word.toLowerCase(); // приведем слово к нижнему регистру
+                        PageEntry pageEntry = new PageEntry(file.getName(), page.getDocument().getPageNumber(page), freqs.get(word));
+                        List<PageEntry> pageEntryList = index.get(word);
+                        if (pageEntryList == null) {
+                            pageEntryList = new ArrayList<>();
+                            index.put(word, pageEntryList);// добавляем pageEntryList в HashMap<>()
+                        }
+                        pageEntryList.add(pageEntry);
+                        Collections.sort(pageEntryList);
+                    }
+//                    System.out.println(index);
+
+                }
+                doc.close();
+                reader.close();
+            }
+        }
     }
 
     @Override
     public List<PageEntry> search(String word) {
-        // тут реализуйте поиск по слову
-        return Collections.emptyList();
+        List<PageEntry> pageEntries = index.get(word);
+        if (pageEntries == null) {
+            return Collections.emptyList();
+        } else {
+            return pageEntries;
+        }
     }
+
 }
+
--- src/main/java/PageEntry.java
+++ src/main/java/PageEntry.java
@@ -3,5 +3,49 @@ public class PageEntry implements Comparable<PageEntry> {
     private final int page;
     private final int count;
 
-    // ???
+
+    public PageEntry(String pdfName, int page, int count) {
+        this.pdfName = pdfName;
+        this.page = page;
+        this.count = count;
+    }
+
+//    public PageEntry(){
+//        this.pdfName = getPdfName();
+//        this.page  = getPage();
+//        this.count = getCount();
+//    }
+
+    public String getPdfName() {
+        return pdfName;
+    }
+
+    public int getPage() {
+        return page;
+    }
+
+    public int getCount() {
+        return count;
+    }
+
+    @Override
+    public String toString() {
+        return "PageEntry{" +
+                "pdfName='" + pdfName + '\'' +
+                ", page=" + page +
+                ", count=" + count +
+                '}';
+    }
+
+    @Override
+    public int compareTo(PageEntry o) {
+        if (o.count > count) {
+            return 1; // возвращаем положительное число, чтобы отсортировать текущий объект после объекта o
+        } else if (o.count < count) {
+            return -1; // возвращаем отрицательное число, чтобы отсортировать текущий объект перед объектом o
+        }
+        return 0; // возвращаем ноль, если значения count равны
+    }
+
+
 }
